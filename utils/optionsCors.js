0026bfdf2f7019dc37402b45b9d8ddfd68b05f12
Roman Shatskikh <shatskikh.roman@gmail.com>
2022-09-15 01:10:21 +0300

    fix: correct localhost in optionCors

--- utils/optionsCors.js
+++ utils/optionsCors.js
@@ -3,8 +3,8 @@ const { STATUS_CODE } = require('./responseInfo');
 // Настройка опций для CORS
 module.exports.optionsCors = {
   origin: [
-    'http://localhost:3000',
-    'localhost:3000',
+    'http://localhost:3001',
+    'localhost:3001',
     'https://movies-search.shatskikh.nomoredomains.sbs',
     'http://movies-search.shatskikh.nomoredomains.sbs',
     'https://api.nomoreparties.co/beatfilm-movies',
